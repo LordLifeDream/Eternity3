4875188939a8e74269f78c3829ee14cc6032300b
LLD <122558744+LordLifeDream@users.noreply.github.com>
2026-01-25 21:21:40 +0100

    add icon

--- src/main/java/eternity3/gui/EternityFrame.java
+++ src/main/java/eternity3/gui/EternityFrame.java
@@ -3,7 +3,11 @@ package eternity3.gui;
 import eternity3.AppManager;
 import eternity3.Eternity;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.Objects;
 
 public class EternityFrame extends JFrame {
 
@@ -19,6 +23,8 @@ public class EternityFrame extends JFrame {
         this.setContentPane(new EternityMainPanel());
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         addMenuBar();
+        this.setTitle("Eternity 3");
+        this.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getClassLoader().getResource("images/icon_superSmall.png"))).getImage());
         this.setVisible(true);
     }
 
@@ -52,6 +58,8 @@ public class EternityFrame extends JFrame {
 
     private void addEternityCategory(JMenuBar b){
         JMenu eternity = new JMenu("Eternity");
+        ImageIcon eternityIcon = new ImageIcon(getClass().getClassLoader().getResource("images/icon_superSmall.png"));
+        eternity.setIcon(eternityIcon);
         //dispose
         JMenuItem dispose = new JMenuItem("dispose window");
         dispose.addActionListener((e)->Eternity.disposeUI());
